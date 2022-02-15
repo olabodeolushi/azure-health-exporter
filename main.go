e412e3a5ca64d95f6f377ce04b6360658033aaa7
Olabode Olushi <olabodeolushi@gmail.com>
2022-02-15 02:16:59 -0500

    Update main.go

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/common/log"
-	"github.com/prometheus/common/version"
+	//"github.com/prometheus/common/log"
+	//"github.com/prometheus/common/version"
 
 	"gopkg.in/alecthomas/kingpin.v2"
 	"gopkg.in/yaml.v2"
